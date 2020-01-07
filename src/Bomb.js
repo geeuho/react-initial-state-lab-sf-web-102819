add7d8f51117f1f9dbb8cbd3cb19cbf6425fb567
Allen Shin <geeuho@gmail.com>
2020-01-07 15:08:42 -0800

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
 // your Bomb code here!
+import React from 'react';
+
+export default class Bomb extends React.Component {
+    constructor(props){
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    timer = () => {
+        let newTime = this.state.secondsLeft - 1
+        this.setState({
+            secondsLeft: newTime
+        })
+    }
+    
+    render(){
+        if (this.state.secondsLeft > 0){
+            setTimeout(this.timer, 1000)
+        }
+        return (
+            <div>{this.state.secondsLeft === 0 ? "Boom!" : `${this.state.secondsLeft} seconds left before I go boom!`}</div>
+        )
+    }
+}
+
+
